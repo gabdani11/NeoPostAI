5ea3c7592775783563cf6d1abaffd5921c635efd
Daniel <gabrielang56799@gmail.com>
2026-07-15 00:44:33 +0530

    fixed ui

--- apps/web/src/app/layouts/components/navigationSidePanel/SidePanel.jsx
+++ apps/web/src/app/layouts/components/navigationSidePanel/SidePanel.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import './sidepanel.scss'
+import React from "react";
+import "./sidepanel.scss";
 const SidePanel = () => {
   return (
-    <aside className='sidePanel'>
-        <nav>
-            <ol>
-                <li>Discover</li>
-                <li>Create</li>
-                <li>Schedule</li>
-            </ol>
-        </nav>
-
+    <aside className="sidePanel">
+      <nav>
+        <ol>
+          <li>Discover</li>
+          <li>Create</li>
+          <li>Schedule</li>
+        </ol>
+      </nav>
+      <div className="setting">
+        <span>settings</span>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
-export default SidePanel
+export default SidePanel;
